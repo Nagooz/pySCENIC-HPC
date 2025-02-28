b734be0b33d5588c65a83d18fd44b2019069a81b
Nagooz <mx.lab@hotmail.fr>
2025-02-28 15:23:00 -0500

    modify bash script to wget all the auxiliary datasets needed for SCENIC

--- modules/download_aux_datasets.sh
+++ modules/download_aux_datasets.sh
@@ -9,11 +9,16 @@ feather_database_urls=(
     'https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-500bp-upstream-7species.mc9nr.genes_vs_motifs.rankings.feather'
     'https://resources.aertslab.org/cistarget/databases/mus_musculus/mm9/refseq_r45/mc9nr/gene_based/mm9-tss-centered-5kb-7species.mc9nr.genes_vs_motifs.rankings.feather'
     )
+motifs_url='https://resources.aertslab.org/cistarget/motif2tf/motifs-v9-nr.mgi-m0.001-o0.0.tbl'
+tfs_url='https://resources.aertslab.org/cistarget/tf_lists/allTFs_mm.txt'
 
 # Loop through the list and download each feather database
 for url in "${feather_database_urls[@]}"; do
   echo "Downloading $url..."
   wget "$url" -P ./data
 done
+
+wget $motifs_url -P ./data
+wget $tfs_url -P ./data
 
 echo "All downloads complete!"
